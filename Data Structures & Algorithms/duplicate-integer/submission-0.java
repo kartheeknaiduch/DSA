c1305bd310133ade22c0ee2b50a9ed84a8a9210f
kartheeknaiduch <166283069+kartheeknaiduch@users.noreply.github.com>
2026-07-22 08:53:39 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> s=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(!s.contains(nums[i]))
+            s.add(nums[i]);
+            else
+            return true;
+        }
+        return false;
+    }
+}
